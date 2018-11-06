b37bf83487a3caf8b1db3031fe16d3ff3ba476d0
Camille LE LUET <cleluet@infinitesquare.com>
2018-11-06 15:07:18 +0100

    Picker

---
+++ src/Controls/Picker/AvailableValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IS.Toolkit.XamarinForms.Controls
+{
+    public class AvailableValue
+    {
+        public string Label { get; set; }
+        public object Value { get; set; }
+    }
+}
