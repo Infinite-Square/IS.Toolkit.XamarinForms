d9407a69894383a8d47dd16ae2a91e563c19298e
DamienDOUMER <damientohin@gmail.com>
2018-12-10 14:09:23 +0100

    I added basic checkbox custom renderer on Android

---
+++ sample/Sample.Android/Renderers/CheckboxRenderer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using IS.Toolkit.XamarinForms.Controls;
+using Sample.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ISCheckbox), typeof(CheckboxRenderer))]
+namespace Sample.Droid.Renderers
+{
+    public class CheckboxRenderer : ViewRenderer<ISCheckbox, CheckBox>
+    {
+        private ISCheckbox RenderedCheckBox => Element as ISCheckbox;
+
+        public CheckboxRenderer(Context context)
+            : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ISCheckbox> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    UpdateAccentColor();
+                }
+
+                Control.Checked = RenderedCheckBox.IsChecked;
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName.Equals(nameof(ISCheckbox.IsChecked)))
+            {
+                Control.Checked = Element.IsChecked;
+            }
+            else if (e.PropertyName.Equals(nameof(ISCheckbox.AccentColor)))
+            {
+                UpdateAccentColor();
+            }
+        }
+
+        private void UpdateAccentColor()
+        {
+            Control?.Background?.SetColorFilter(RenderedCheckBox.AccentColor.ToAndroid(), Android.Graphics.PorterDuff.Mode.SrcAtop);
+        }
+    }
+}
---
+++ src/Controls/Checkbox/ISCheckbox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace IS.Toolkit.XamarinForms.Controls
+{
+    public class ISCheckbox : View
+    {
+        public static readonly BindableProperty IsCheckedBindableProperty = BindableProperty.Create(
+                                                        nameof(IsChecked),
+                                                        typeof(bool),
+                                                        typeof(ISCheckbox),
+                                                        false,
+                                                        BindingMode.TwoWay);
+
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedBindableProperty);
+            set => SetValue(IsCheckedBindableProperty, value);
+        }
+
+        public static readonly BindableProperty AccentColorProperty = BindableProperty.Create(nameof(AccentColor), typeof(Color), typeof(ISCheckbox), Color.CadetBlue);
+        public Color AccentColor
+        {
+            get { return (Color)GetValue(AccentColorProperty); }
+            set { SetValue(AccentColorProperty, value); }
+        }
+
+        public static readonly BindableProperty CheckedCommandProperty = BindableProperty.Create(nameof(CheckedCommand), typeof(ICommand), typeof(ISCheckbox), null);
+        public ICommand CheckedCommand
+        {
+            get => (ICommand)GetValue(CheckedCommandProperty);
+            set => SetValue(CheckedCommandProperty, value);
+        }
+
+        public static readonly BindableProperty CheckedCommandArguementProperty = BindableProperty.Create(nameof(CheckedCommandArguement), typeof(object), typeof(ISCheckbox), default(object));
+        public object CheckedCommandArguement
+        {
+            get => (object)GetValue(CheckedCommandArguementProperty);
+            set => SetValue(CheckedCommandArguementProperty, value);
+        }
+    }
+}
